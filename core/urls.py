55dbd572d6f6d8ea41b1034d5b6cb537df76b6bf
KereeceWilliams <kereece.williams@quinnipiac.edu>
2015-12-07 18:22:07 +0000

    Add Login

--- core/urls.py
+++ core/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns('',
                        url(r'^review/update/(?P<pk>\d+)/$', ReviewUpdateView.as_view(), name='review_update'),
        url(r'^review/delete/(?P<pk>\d+)/$', ReviewDeleteView.as_view(), name='review_delete'),
                        url(r'^review/(?P<pk>\d+)/comment/create/$', CommentCreateView.as_view(), name='comment_create'),
-                       url(r'^question/(?P<question_pk>\d+)/answer/update/(?P<answer_pk>\d+)/$', AnswerUpdateView.as_view(), name='answer_update'),
+                       url(r'^review/(?P<review_pk>\d+)/comment/update/(?P<comment_pk>\d+)/$', CommentUpdateView.as_view(), name='comment_update'),
                       )
